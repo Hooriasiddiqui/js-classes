f3230f79aabacc903d65b0fad7e47c1796aceac1
Hooriasiddiqui <hoorianaseem05@gmail.com>
2024-03-10 13:24:42 +0500

    learning

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// "" -> Double Quotation
+// '' -> single Quotation
+// ; -> terminator
+// : -> colons
+// `` -> backtick
+
+// variable name meaningful
+// space not allowed user -> underscore -
+// camel case  databaseName
+// case sensitive 
+
+// var replace/update/same name
+
+// es6
+// let update/replace/ 
+// const // this is constant
+
+ //var test = "javascript";
+  //document.write(test);
+
+// var test = "programming";
+ // document.write(test);
+
+//    let test2 = "i love javascript";
+  // document.write(test2);
+//    let test2 = "js"
+ // document.write(test2);
+
+ // const test3 = "this is constant";
+// test3 = "hello";
+ // document.write(test3);
+
+ // + is use for concatenation operator in string
